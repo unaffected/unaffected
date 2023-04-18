82bdab46f616a9e8f2545fc53ae3f05077e719d3
Cody Mercer <me@codymercer.com>
2023-04-18 18:24:07 -0500

    chore(build): remove types from vcs

--- package/command/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Application from '@unaffected/app';
-export interface Command<T = never> {
-    id: string;
-    execute: (app: Application) => (data: T) => Promise<any>;
-}
-declare const app: Application;
-export default app;
-//# sourceMappingURL=index.d.ts.map
