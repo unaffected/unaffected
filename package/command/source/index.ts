ccfab12e510b4e53f5d9f583636021f9ff7e0cd7
Cody Mercer <me@codymercer.com>
2023-04-18 18:07:06 -0500

    feat(package/command): update command typings

--- package/command/source/index.ts
+++ package/command/source/index.ts
@@ -1,13 +1,23 @@
 import Application from '@unaffected/app'
 
-export type Context<Input = never, Output = void> = {
-  input: Partial<Input>
-  output: Partial<Output>
+export interface Command<Input = never, Output = void, P extends Params = never> {
+  id: string
+  execute: Execute<Input, Output, P>
 }
 
-export interface Command<Input = never, Output = void> {
-  id: string
-  execute: (app: Application) => (data: Input) => Promise<Output>
+export type Context<Input = never, Output = void, Params extends Record<string, any> = never> = {
+  app: Application
+  input: Partial<Input>
+  output: Partial<Output>
+  params: Params
 }
+
+export type Execute<
+  Input = never,
+  Output = void,
+  Params extends Record<string, any> = never
+> = (context: Context<Input, Output, Params>) => Promise<Output>
+
+export interface Params {}
 
 export default {}
