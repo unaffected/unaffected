228e6b13459914af8436733bf113bb5d3a041ad8
Cody Mercer <me@codymercer.com>
2023-04-18 21:35:11 -0500

    feat(command): add authorize property (#1)
    * feat(command): add authorize property
    
    * test(ci): use correct github action
    
    * test(ci): rename workflow job
    
    * fix(command): update package lock file
    
    * build: fix module resolution

--- package/utility/source/index.ts
+++ package/utility/source/index.ts
@@ -1,3 +1,4 @@
+export * as guard from '@unaffected/utility/guard'
 export * as pattern from '@unaffected/utility/pattern'
 export { default as Store } from '@unaffected/utility/store'
 export * as timer from '@unaffected/utility/timer'
