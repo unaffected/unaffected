65fd3eb11c7c314c735116bfec44d28e518a19d1
Cody Mercer <me@codymercer.com>
2023-04-18 20:10:50 -0500

    feat(utility/store): refactor utility module

--- package/utility/source/store.test.ts
+++ package/utility/source/store.test.ts
@@ -32,4 +32,151 @@ describe('utility: store', () => {
       expect(store.get()).toBe(state)
     })
   })
+
+  test('getting the entries for a store', () => {
+    const store = new Store({ foo: 'bar', bar: 'baz' })
+
+    const expected = expect.arrayContaining([
+      ['foo', 'bar'],
+      ['bar', 'baz'],
+    ])
+
+    expect(store.entries).toEqual(expected)
+  })
+
+  describe('determining a values type', () => {
+    const store = new Store({
+      array: ['foo'],
+      array_2: ['foo'],
+      date: new Date(),
+      date_2: new Date(),
+      boolean: true,
+      boolean_2: true,
+      number: 1,
+      number_2: 1,
+      object: { foo: 'bar' },
+      object_2: { foo: 'bar' },
+    })
+
+    test('determining if a value is an array', () => {
+      expect(store.is('array', 'array')).toBeTruthy()
+      expect(store.is('array', 'boolean')).toBeFalsy()
+      expect(store.is_array('array')).toBeTruthy()
+      expect(store.is_array('boolean')).toBeFalsy()
+    })
+
+    test('determining if all values are arrays', () => {
+      expect(store.is_array(['array', 'array_2'])).toBeTruthy()
+      expect(store.is_array(['array', 'boolean'])).toBeFalsy()
+    })
+
+    test('determining if a value is a boolean', () => {
+      expect(store.is('boolean', 'boolean')).toBeTruthy()
+      expect(store.is('boolean', 'array')).toBeFalsy()
+      expect(store.is_boolean('boolean')).toBeTruthy()
+      expect(store.is_boolean('array')).toBeFalsy()
+    })
+
+    test('determining if all values are booleans', () => {
+      expect(store.is_boolean(['boolean', 'boolean_2'])).toBeTruthy()
+      expect(store.is_boolean(['boolean', 'array'])).toBeFalsy()
+    })
+
+    test('determining if a value is a number', () => {
+      expect(store.is('number', 'number')).toBeTruthy()
+      expect(store.is('boolean', 'array')).toBeFalsy()
+      expect(store.is_number('number')).toBeTruthy()
+      expect(store.is_number('array')).toBeFalsy()
+    })
+
+    test('determining if all values are numbers', () => {
+      expect(store.is_number(['number', 'number_2'])).toBeTruthy()
+      expect(store.is_number(['number', 'boolean'])).toBeFalsy()
+    })
+
+    test('determining if a value is an object', () => {
+      expect(store.is('object', 'object')).toBeTruthy()
+      expect(store.is('object', 'number')).toBeFalsy()
+      expect(store.is_object('object')).toBeTruthy()
+      expect(store.is_object('number')).toBeFalsy()
+    })
+
+    test('determining if all values are objects', () => {
+      expect(store.is_object(['object', 'object_2'])).toBeTruthy()
+      expect(store.is_object(['object', 'boolean'])).toBeFalsy()
+    })
+
+    test('determining if a value is a date', () => {
+      expect(store.is('date', 'date')).toBeTruthy()
+      expect(store.is('date', 'number')).toBeFalsy()
+      expect(store.is_date('date')).toBeTruthy()
+      expect(store.is_date('number')).toBeFalsy()
+    })
+
+    test('determining if all values are dates', () => {
+      expect(store.is_date(['date', 'date_2'])).toBeTruthy()
+      expect(store.is_date(['date', 'boolean'])).toBeFalsy()
+    })
+
+    test('determining if a value is an undefined', () => {
+      expect(store.is('undefined', 'undefined')).toBeTruthy()
+      expect(store.is('undefined', 'number')).toBeFalsy()
+      expect(store.is_undefined('undefined')).toBeTruthy()
+      expect(store.is_undefined('number')).toBeFalsy()
+    })
+
+    test('determining if all values are undefined', () => {
+      expect(store.is_undefined(['undefined', 'undefined_2'])).toBeTruthy()
+      expect(store.is_undefined(['undefined', 'boolean'])).toBeFalsy()
+    })
+  })
+
+  describe('resetting the store state', () => {
+    const store = new Store({
+      foo: 'bar',
+      bar: 'baz',
+      baz: 'foo',
+    })
+
+    test('resetting a single key', () => {
+      store.set('foo', 'baz')
+
+      store.reset('foo')
+
+      expect(store.get('foo')).toBe('bar')
+      expect(store.get('bar')).toBe('baz')
+      expect(store.get('baz')).toBe('foo')
+    })
+
+    test('resetting a multiple keys', () => {
+      store.set('foo', 'baz')
+      store.set('bar', 'foo')
+
+      store.reset(['foo', 'bar'])
+
+      expect(store.get('foo')).toBe('bar')
+      expect(store.get('bar')).toBe('baz')
+      expect(store.get('baz')).toBe('foo')
+    })
+
+    test('resetting all keys', () => {
+      store.set('foo', 'baz')
+      store.set('bar', 'foo')
+
+      store.reset()
+
+      expect(store.get('foo')).toBe('bar')
+      expect(store.get('bar')).toBe('baz')
+      expect(store.get('baz')).toBe('foo')
+    })
+  })
+
+  test('replacing the store state', () => {
+    const store = new Store({ foo: 'bar', bar: 'baz' })
+
+    store.replace({ foo: 'baz', bar: 'foo' })
+
+    expect(store.get('foo')).toBe('baz')
+    expect(store.get('bar')).toBe('foo')
+  })
 })
--- package/utility/source/store.ts
+++ package/utility/source/store.ts
@@ -1,4 +1,5 @@
-import { get, set, mergeWith, omit, pick } from '@unaffected/utility'
+import { get, set, mergeWith, omit, pick, timer } from '@unaffected/utility'
+import type { Timer } from '@unaffected/utility/timer'
 
 export type State<T extends Record<string, any> = Record<string, any>> = T
 
@@ -10,14 +11,8 @@ export interface Timeout<T extends string> {
   timer: NodeJS.Timer | number
   cancel: () => void
 }
-
-export type Timeouts<T extends string> = Map<T, Timeout<T>>
-
-export type Timer<T extends string> = Omit<Timeout<T>, 'timer'> & { remaining: number }
-export type Timers<T extends string> = Partial<Record<T, Timer<T>>>
-
 export class Store<T extends State> {
-  private readonly timeouts: Timeouts<string>
+  private readonly timeouts: Map<string, Timer>
   private readonly initial: Partial<T>
   private state: Partial<T>
 
@@ -27,34 +22,16 @@ export class Store<T extends State> {
     this.state = state
   }
 
-  /**
-   * Retrieve all the key-value pairs in
-   * the store as a collection of tuples.
-   */
   get entries() {
     return Object.entries(this.state)
   }
 
-  /**
-   * Retrieve all the keys in from the store.
-   */
   get keys() {
     return Object.keys(this.state)
   }
 
-  /**
-   * Retrieve the store's active expiration timers.
-   */
-  get timers(): Timers<string> {
-    return Array
-      .from(this.timeouts.values())
-      .reduce((acc, { timer, ...timeout }) => ({
-        ...acc,
-        [timeout.key]: {
-          ...timeout,
-          remaining: timeout.expiration - (Date.now() - timeout.created_at),
-        },
-      }), {})
+  get timers(): Record<string, Timer> {
+    return Object.fromEntries(this.timeouts.entries())
   }
 
   get values() {
@@ -69,9 +46,6 @@ export class Store<T extends State> {
     return this.get(key) === value
   }
 
-  /**
-   * Set a duration-based expiration for the given key(s).
-   */
   expire(key: string | string[], expiration: number) {
     if (Array.isArray(key)) {
       key.forEach((k) => this.expire(k, expiration))
@@ -79,17 +53,7 @@ export class Store<T extends State> {
       return this
     }
 
-    const created_at = Date.now()
-    const timer = setTimeout(() => { this.remove(key) }, expiration)
-
-    const timeout: Timeout<string> = {
-      key,
-      expiration,
-      timer,
-      created_at,
-      expires_at: created_at + expiration,
-      cancel: () => clearTimeout(timer),
-    }
+    const timeout = timer.create(() => { this.remove(key) }, expiration)
 
     this.keep(key)
 
@@ -98,9 +62,6 @@ export class Store<T extends State> {
     return this
   }
 
-  /**
-   * Set a date-based expiration for the given key(s).
-   */
   expire_at(key: string | string[], expire_at: number | Date) {
     const time = typeof expire_at === 'number' ? expire_at : expire_at.getTime()
     const expiration = time - Date.now()
@@ -112,9 +73,6 @@ export class Store<T extends State> {
     return this.expire(key, expiration)
   }
 
-  /**
-   * Determine if a non-undefined value exists at the given key(s).
-   */
   exists(key: string | string[]): boolean {
     if (Array.isArray(key)) {
       return key.every(this.exists.bind(this))
@@ -126,10 +84,6 @@ export class Store<T extends State> {
     ].every(Boolean)
   }
 
-  /**
-   * Retrieve the value at the given key.
-   * If the value is undefined the fallback will be returned instead.
-   */
   get(key?: string, fallback?: any): State<T> | any {
     if (key) {
       return get(this.state, key) ?? fallback
@@ -138,9 +92,6 @@ export class Store<T extends State> {
     return this.state
   }
 
-  /**
-   * Determine if the value of a specific key is the given type.
-   */
   is(key: string | string[], type: any): boolean {
     if (Array.isArray(key)) {
       return key.every((k) => this.is(k, type))
@@ -163,62 +114,34 @@ export class Store<T extends State> {
     return value === type
   }
 
-  /**
-   * Determine if the value at the given key is an array.
-   * @param key string
-   * @returns {boolean}
-   */
   is_array(key: string | string[]) {
     return this.is(key, 'array')
   }
 
-  /**
-   * Determine if the value at the given key is a boolean.
-   * @param key string
-   * @returns {boolean}
-   */
   is_boolean(key: string | string[]) {
     return this.is(key, 'boolean')
   }
 
-  /**
-   * Determine if the value at the given key is a date.
-   */
   is_date(key: string | string[]) {
     return this.is(key, 'date')
   }
 
-  /**
-   * Determine if the value at the given key is an object.
-   */
   is_object(key: string | string[]) {
     return this.is(key, 'object')
   }
 
-  /**
-   * Determine if the value at the given key is a number.
-   */
   is_number(key: string | string[]) {
     return this.is(key, 'number')
   }
 
-  /**
-   * Determine if the value at the given key is a string.
-   */
   is_string(key: string | string[]) {
     return this.is(key, 'string')
   }
 
-  /**
-   * Determine if the value at the given key is undefined.
-   */
   is_undefined(key: string | string[]) {
     return this.is(key, 'undefined')
   }
 
-  /**
-   * Cancel any expiration timeouts for the given key(s).
-   */
   keep(key: string | string[]) {
     if (Array.isArray(key)) {
       key.forEach(this.keep.bind(this))
@@ -235,9 +158,6 @@ export class Store<T extends State> {
     return this
   }
 
-  /**
-   * Recursively merge the given state with the current state.
-   */
   merge(state: Partial<T>) {
     this.state = mergeWith(this.state, state, (existing, mutation) => {
       if (Array.isArray(existing)) {
@@ -250,16 +170,10 @@ export class Store<T extends State> {
     return this
   }
 
-  /**
-   * Retrieve an object of key-value pairs for the given keys.
-   */
   pick(key: string | string[] = []) {
     return pick(this.state, key as keyof State<T>)
   }
 
-  /**
-   * Refresh the expiration timer for the given key(s).
-   */
   refresh(key: string | string[]) {
     if (Array.isArray(key)) {
       key.forEach(this.refresh.bind(this))
@@ -270,35 +184,24 @@ export class Store<T extends State> {
     const timeout = this.timeouts.get(key)
 
     if (timeout) {
-      timeout.cancel()
-      this.expire(timeout.key, timeout.expiration)
+      timeout.reset()
     }
 
     return this
   }
 
-  /**
-   * Remove the given key(s).
-   */
   remove(key: string | string[]) {
     this.state = omit(this.state, key) as Partial<T>
 
     return this
   }
 
-  /**
-   * Replace the current state.
-   */
   replace(state: Partial<T>) {
     this.state = state
 
     return this
   }
 
-  /**
-   * Reset the state of the given key(s).
-   * Reset the entire store if no keys are provided.
-   */
   reset(key?: string | string[]) {
     if (Array.isArray(key)) {
       key.forEach(this.reset.bind(this))
@@ -317,10 +220,6 @@ export class Store<T extends State> {
     return this
   }
 
-  /**
-   * Set the value of the given key.
-   * The key will expire automatically if an expiration is provided.
-   */
   set(key: string, value?: any, expiration?: number) {
     set(this.state, key, value)
 
@@ -331,10 +230,6 @@ export class Store<T extends State> {
     return this
   }
 
-  /**
-   * Retrieve the value for the given key(s), and
-   * remove any retrieved keys from the store.
-   */
   take(key: string | string[], fallback?: any) {
     const value = Array.isArray(key) ? this.pick(key) : this.get(key)
 
@@ -343,16 +238,10 @@ export class Store<T extends State> {
     return value
   }
 
-  /**
-   * Retrieve the expiration timer for the given key.
-   */
   timer(key: string) {
     return this.timers[key]
   }
 
-  /**
-   * Determine the type of the value stored at the given key.
-   */
   type(key: string) {
     return typeof this.get(key)
   }
