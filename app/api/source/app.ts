4a34e50b9dd45fda402bd3baefc74c27e5ce46a9
Cody Mercer <me@codymercer.com>
2023-04-21 00:15:38 -0500

    feat(api): use new http transport

--- app/api/source/app.ts
+++ app/api/source/app.ts
@@ -1,10 +1,15 @@
 import Application from '@unaffected/app'
-import plugins from '@unaffected/api/plugin/index'
+import gateway from '@unaffected/api/plugin/gateway'
 
 declare module '@unaffected/app' { interface Application { ready: Promise<boolean> } }
 
 export const app = new Application()
 
-await app.configure(plugins)
+await app.configure(gateway)
+
+app.channel.on('unaffected:gateway:http:request', ({ id, response }) => {
+  console.log('event:', id)
+  response.end(`event: ${id}`)
+})
 
 export default app
--- app/api/source/plugin/gateway.ts
+++ app/api/source/plugin/gateway.ts
@@ -1,38 +1,13 @@
 import { type Plugin } from '@unaffected/app'
-import gateway, { UWS } from '@unaffected/gateway/plugin'
-import uuid from '@unaffected/utility/uuid'
-
-export const EVENT = {
-  CONNECTED: 'gateway:socket:connected',
-  DISCONNECTED: 'gateway:socket:disconnected',
-  DRAIN: 'gateway:socket:drain',
-  MESSAGE: 'gateway:socket:message',
-  REQUEST: 'gateway:http:request',
-} as const
+import gateway from '@unaffected/gateway/plugin'
+import http from '@unaffected/gateway/plugin/transport/http'
 
 export const plugin: Plugin = {
   id: 'unaffected:api:gateway',
   install: async (app) => {
     await app.configure(gateway)
-
-    app
-      .gateway
-      .ws('/socket', {
-        compression: UWS.DEDICATED_COMPRESSOR_3KB,
-        idleTimeout: 30,
-        maxBackpressure: 1024,
-        maxPayloadLength: 512,
-        drain: (socket) => { app.channel.publish(EVENT.DRAIN, { socket }) },
-        message: (socket, message, is_binary) => { app.channel.publish(EVENT.MESSAGE, { is_binary, message, socket }) },
-        close: (socket) => { app.channel.publish(EVENT.DISCONNECTED, socket) },
-        open: (socket) => { app.channel.publish(EVENT.CONNECTED, socket) },
-      })
-      .any('/*', (response, request) => {
-        app.channel.publish(EVENT.REQUEST, { id: uuid(), request, response })
-      })
+    await app.configure(http)
   },
 }
-
-export { UWS }
 
 export default plugin
