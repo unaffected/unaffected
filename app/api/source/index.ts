ef22e6ca22de485e4aebd72efcb71e9f78e091e2
Cody Mercer <me@codymercer.com>
2023-04-21 19:42:00 -0500

    wip(api): remove existing code

--- app/api/source/index.ts
+++ app/api/source/index.ts
@@ -1,11 +1,1 @@
-import app from '@unaffected/api/app'
-
-const PORT = 9001 as const
-
-app.gateway.listen(PORT, (token) => {
-  if (token) {
-    console.info('listening on port:', PORT)
-  } else {
-    console.error('failed to start gateway')
-  }
-})
+export default {}
